f9410e3a58edb158073902898faf76ed95d164f0
Logic-Gun <logic>
2025-02-11 14:01:19 +0000

    sex moment

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ public class Plugin : Plugin<Config>
     public override string Name => "Auto Cleaner";
     public override string Description => "Allows you to clear the map every given n seconds.";
     public override string Author => "Logic_Gun";
-    public override Version Version => new(1, 0, 1);
+    public override Version Version => new(1, 0, 2);
     public override Version RequiredApiVersion => new(0, 0, 0); // idk labapi version lol
 
     public ServerHandler ServerHandler => new();
